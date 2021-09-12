7c1b3f1e8460f9a61acf1a046300a3b40d5df4d2
Roman Khromets <79381229+rhromets@users.noreply.github.com>
2021-09-13 00:54:37 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,15 @@
 import firebase from "firebase/compat/app";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const firebaseInit = firebase.initializeApp({
-  apiKey: "AIzaSyC3-EvrEFUHCsenmkFDNQHaO_nigNp3Ry0",
-  authDomain: "chat-frontend-1e43d.firebaseapp.com",
-  projectId: "chat-frontend-1e43d",
-  storageBucket: "chat-frontend-1e43d.appspot.com",
-  messagingSenderId: "581438292796",
-  appId: "1:581438292796:web:338d9673b16e39307a9439",
+  apiKey: process.env.REACT_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 });
 
 export default firebaseInit;
